170d8e6688a9560d1c66a54886ed3bc1ee2296c2
Norhidayah <75527623+hidayahRB@users.noreply.github.com>
2021-06-12 04:03:11 +0800

    depot class

---
+++ Source code/Depot.java
@@ -0,0 +1,7 @@
+public class Depot extends Stop{
+    //id for depot always '0', and only one depot for entire program
+    public Depot(int x, int y, int capacity){
+        super(0, x , y, capacity);
+        visited = true;
+    }
+}
