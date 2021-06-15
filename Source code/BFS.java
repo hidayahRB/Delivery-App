2a63805a005888cc613461a023d81d045a9c9436
Norhidayah <75527623+hidayahRB@users.noreply.github.com>
2021-06-15 22:33:50 +0800

    Finalized BFS Simulation

--- Source code/BFS.java
+++ Source code/BFS.java
@@ -1,62 +1,275 @@
-import java.util.*;
+package delivery_app;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class BFS {
-    private int V;   // No. of vertices
-    private LinkedList<Integer> adj[]; //Adjacency Lists
+    
+    private Queue<Node> queue;
+    private Node firstNode;
+    
+    private double tourCost = 0;
+    private final int riderCapacity;
 
+    private String routeOutput = "";
+    
+    static class Node
+    {
+        Stop data;
+        List<Node> neighbours;
+        
+        Node(Stop stop){ //stop means either Depot/Customer
+            data = stop;
+            neighbours = new ArrayList<>();
+        }
+        
+        public void addneighbours(Node neighbourNode){
+            this.neighbours.add(neighbourNode);
+        }
+        
+        public List<Node> getNeighbours() {
+            return neighbours;
+        }
+        
+        public void setNeighbours(List<Node> neighbours) {
+            this.neighbours = neighbours;
+        }
+        
+        public double calculateDistance(int nextCustomerX, int nextCustomerY){
+            return data.calculateCost(nextCustomerX, nextCustomerY);
+        }
+        
+        //Getter and Setter method (extract from Stop class)
+        public int getX(){
+            return data.x;
+        }
+        
+        public int getY(){
+            return data.y;
+        }
+        
+        public int getID(){
+            return data.getID();
+        }
+        
+        public int getCapacity(){
+            return data.getCapacity();
+        }
+        
+        public boolean isChecked(){
+            return data.isChecked();
+        }
+        
+        public boolean isVisited(){
+            return data.isVisited();
+        }
+        
+        public void setChecked(boolean checked) {
+            data.setChecked(checked);
+        }
+
+        public void setVisited(boolean visited) {
+            data.setVisited(visited);
+        }
+        
+    }  
+    
     // Constructor
-    public BFS(int v)
-    {
-        V = v;
-        adj = new LinkedList[v];
-        for (int i=0; i<v; ++i)
-            adj[i] = new LinkedList();
+    public BFS(Node node, int riderCapacity){
+        queue = new LinkedList<>();
+        firstNode = node;
+        this.riderCapacity = riderCapacity;
+        
+        bfs(node);
     }
- 
-    // Function to add an edge into the graph
-    public void addEdge(int v,int w)
+    
+    private void bfs(Node depot)
     {
-        adj[v].add(w);
-    }
+        queue.add(depot);
+        depot.setVisited(true);
+        
+        Route newRoute = null;
+        
+        boolean isAnyNodeQualified = false;
+        int currentUsedCapacity = 0;
+        
+        int routeCounter = 0;
+        
+        while ( !isAllNodeVisited() ) {
  
-    // prints BFS traversal from a given source s
-    public void printBFS(int s)
-    {
-        // Mark all the vertices as not visited(By default
-        // set as false)
-        boolean visited[] = new boolean[V];
- 
-        // Create a queue for BFS
-        LinkedList<Integer> queue = new LinkedList<Integer>();
- 
-        // Mark the current node as visited and enqueue it
-        visited[s]=true;
-        queue.add(s);
- 
-        while (queue.size() != 0)
-        {
-            // Dequeue a vertex from queue and print it
-            s = queue.poll();
-            System.out.print(s+" ");
- 
-            // Get all adjacent vertices of the dequeued vertex s
-            // If a adjacent has not been visited, then mark it
-            // visited and enqueue it
-            Iterator<Integer> i = adj[s].listIterator();
-            while (i.hasNext())
-            {
-                int n = i.next();
-                if (!visited[n])
-                {
-                    visited[n] = true;
-                    queue.add(n);
+            Node element = queue.remove();
+            if(element.getID() == 0) newRoute = initiateNewRoute();
+            else newRoute.addStopToRoute(element.data);
+            
+            List<Node> neighbours = element.getNeighbours();
+            
+            while(!isAllNeighbourChecked(neighbours)){
+                
+                Node nextCustomer = getNearestNextCustomer(element, neighbours);
+                
+                if(isNodeQualifiedToBeAdded(nextCustomer, currentUsedCapacity)){
+//                    System.out.println("Node " + nextCustomer.getID() + " qualified");
+
+                    currentUsedCapacity += nextCustomer.getCapacity();
+//                    System.out.println("currentUsedCapacity : " + currentUsedCapacity);
+
+                    queue.add(nextCustomer); //update queue for BFS
+                    
+                    nextCustomer.setVisited(true);
+                    isAnyNodeQualified = true;
+                    
+                    break;
+                }
+                else{
+                    nextCustomer.setChecked(true);
                 }
             }
+            
+            //Route capacity already at maximum or all node already visited
+            if(isAnyNodeQualified == false){
+                backToDepot(newRoute);
+                newRoute.setCapacity(currentUsedCapacity);
+                routeCounter++;
+
+                routeOutput += "Route " + routeCounter + "\n" + newRoute;
+
+                tourCost += newRoute.getRouteCost();
+                
+//                System.out.println("New Route");
+                
+                //search from depot (id 0) again, new route
+                queue.add(depot);
+                currentUsedCapacity = 0;
+            }
+            
+            resetCheckStatusForUnvisitedNode(neighbours);
+            isAnyNodeQualified = false;
+        }
+        
+        //if loop end before adding the last 'nextCustomer' to route (should be at line 103)
+        if(!queue.isEmpty()){
+            Node element = queue.remove();
+            newRoute.addStopToRoute(element.data);
+        }
+        
+        //when reaching here, the current route processed still not complete (no back to Depot), so here we add Depot as last Node
+        backToDepot(newRoute);
+        newRoute.setCapacity(currentUsedCapacity);
+        
+        routeCounter++;
+
+        routeOutput += "Route " + routeCounter + "\n" + newRoute;
+        
+        tourCost += newRoute.getRouteCost();
+
+        
+        System.out.println("Tour cost : " + tourCost + "\n");
+        System.out.print(routeOutput);
+
+    }
+    
+    private void backToDepot(Route currentRouteSearched){
+        currentRouteSearched.addStopToRoute(firstNode.data);
+    }
+    
+    private Route initiateNewRoute(){
+        Route newRoute = new Route();
+        newRoute.getStopList().add( (Depot) firstNode.data);
+        
+        return newRoute;
+    }
+    
+    private boolean isAllNeighbourChecked(List<Node> neighbourList){
+        
+        for(Node node : neighbourList){
+            if(node.isVisited()) continue;
+            
+            if(!node.isChecked()) return false;
+        }
+        
+        return true;
+    }
+    
+    private void resetCheckStatusForUnvisitedNode(List<Node> neighbourList){
+        for(Node node : neighbourList){
+            if(node.isVisited()) continue;
+            
+            node.setChecked(false);
         }
     }
     
-    
+    private boolean isAllNodeVisited(){
         
+        for(Node node : firstNode.getNeighbours()){
+            if(!node.isVisited()) return false;
+        }
+        
+        return true;
+    }
     
+    //disebabkan search always start from depot (id 0), next node mesti Customer, hence nama method ni getNearestNextCustomer
+    private Node getNearestNextCustomer(Node currentNodeForCheck, List<Node> neighbourList){
+        Node nearestNextCustomer = null;
+        double dummyForComparison = Integer.MAX_VALUE;
+        
+        for(Node node : neighbourList){
+            if(node.isVisited() || node.isChecked()) continue;
+            
+            double distanceToNextCustomer = currentNodeForCheck.calculateDistance(node.getX(), node.getY());
+            if(distanceToNextCustomer < dummyForComparison){
+                dummyForComparison = distanceToNextCustomer;
+                nearestNextCustomer = node;
+            }
+        }
+        
+//        System.out.println("Nearest Customer for Node " + currentNodeForCheck.getID() + " : " + nearestNextCustomer.getID());
+        
+        return nearestNextCustomer;
+    }
+    
+    private boolean isNodeQualifiedToBeAdded(Node nextNodeForCheck, int currentUsedCapacity){
+        return (currentUsedCapacity + nextNodeForCheck.getCapacity()) <= riderCapacity;
+    }
+    
+//    public static void main(String arg[])
+//    {
+//        
+//        ArrayList<Node> nodeList = new ArrayList<>();
+//        nodeList.add(new Node(new Depot(147, 103, 0)));
+//        nodeList.add(new Node(new Customer(111,98,7)));
+//        nodeList.add(new Node(new Customer(87,176,16)));
+//        nodeList.add(new Node(new Customer(118,98,15)));
+//        nodeList.add(new Node(new Customer(69,168,5)));
+//        nodeList.add(new Node(new Customer(72,84,19)));
+//        nodeList.add(new Node(new Customer(35,79,21)));
+//        nodeList.add(new Node(new Customer(25,115,4)));
+//        nodeList.add(new Node(new Customer(79,139,8)));
+//
+//        constructCompleteUndirectedGraph(nodeList);
+//        
+//        Node depot = nodeList.get(0);
+//        int riderCapacity = 21;
+//        
+//        System.out.println("The BFS traversal of the graph is \n");
+//        
+//        new BFS(depot, riderCapacity);
+//        
+//    }
+//     
+//    private static void constructCompleteUndirectedGraph(List<Node> nodeList){
+//        
+//        for(Node node : nodeList){
+//            
+//            for(Node neighbourNode : nodeList){
+//                if(neighbourNode.equals(node)) continue;
+//                
+//                node.addneighbours(neighbourNode);
+//            }
+//            
+//        }
+//        
+//    }
 }
 
